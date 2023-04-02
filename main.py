89a1ee351d05df30df9b350adff48d46f77a323f
MattG-bci <mateusz.f.grzybowski@gmail.com>
2023-04-02 19:04:20 +0100

    Added interaction between PyGame and a user

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 import pygame
 
 
@@ -12,39 +13,54 @@ if __name__ == "__main__":
 
     path = "./flags"
     flags = os.listdir(path)
+    random.shuffle(flags)
     status = True
 
     correct = 0
     incorrect = 0
     n_flags = len(flags)
     while status:
-
-        for i in pygame.event.get():
+        for event in pygame.event.get():
             for flag in flags:
                 path_flag = path + "/" + flag
                 flag_name = flag.replace(".jpeg", "")
                 flag_img = pygame.image.load(path_flag).convert()
 
-                screen.blit(flag_img, (280, 20))
-                pygame.display.flip()
-
-                answer = input("Whom is this flag? \n")
-                if answer.lower() == flag_name:
-                    print("Correct answer!")
-                    correct += 1
-                    flags.remove(flag)
-
-                    if not flags:
-                        print("Congratulations! You have guessed all the flags!")
-                        print(f"Your score is: {(correct/(n_flags + incorrect) * 100):.3f}%")
-                        status = False
-            
-                else:
-                    print("That's incorrect")
-                    incorrect += 1
-                
-                if i == pygame.QUIT:
+            while True:
+                if not flags:
+                    print("Congratulations! You have guessed all the flags!")
+                    print(f"Your score is: {(correct/(n_flags + incorrect) * 100):.3f}%")
                     status = False
+                    break
+
+                if event.type == pygame.KEYDOWN:
+                    screen.blit(flag_img, (280, 20))
+                    pygame.display.flip()
+
+                    if event.key == pygame.K_SPACE:
+                        break
+
+                    elif event.key == pygame.K_i:
+
+                        answer = input("Whom is this flag? \n")
+                        if answer.lower() == flag_name:
+                            print("Correct answer!")
+                            correct += 1
+                            flags.remove(flag)
+                            break
+            
+                        else:
+                            print("That's incorrect")
+                            incorrect += 1
+                            break
+
+                else:
+                    break
+
+                
+                
+            if event == pygame.QUIT:
+                status = False
                 
     
     pygame.quit()
